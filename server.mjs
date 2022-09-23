8a2d1b4f5b57b02dcc0359bceab63f903ae8c6e3
hamzaali565 <ha6153806@gmail.com>
2022-09-23 18:34:02 +0500

    express server

---
+++ server.mjs
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    console.log("someone is requesting on this server", req.ip);
+    res.send('Hello World from Sir Inzamam Computer!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
