13ff4a5807eda90b5c29ec2ff80c649d394d1404
jatindarkhatry <khatrij784@gmail.com>
2022-10-07 19:49:56 +0500

    todo

--- server.mjs
+++ server.mjs
@@ -1,5 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
+await mongoose.connect('mongodb+srv://Shahmir:password@cluster0.9b2tk3b.mongodb.net/?retryWrites=true&w=majority');
+
+
 
 const app = express()
 const port = process.env.PORT || 3000;
